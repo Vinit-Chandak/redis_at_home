1250c2da3192f09f8eaa75ad149f2f98b184bbbf
Vinit Chandak <defozex47@gmail.com>
2025-02-02 03:00:38 +0530

    phase 2: server and client

--- client.cpp
+++ client.cpp
@@ -1,47 +1,166 @@
-/*
-CLIENT:
-    1. Create a socket
-    2. Connect to the server
-    3. Send a message to the server
-    4. Read the response from the server
-*/
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/socket.h>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
 #include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
+#define LOG_ERROR(fmt)                                                         \
+  (fprintf(stderr, "[ERROR] %s:%d(): " fmt "\n", __FILE__, __LINE__))
+
+// max request size
+const size_t MAX_MSG_SIZE = 4096;
+
+static bool isLittleEndian() {
+  uint32_t test = 1;
+  return (*(unsigned char *)&test) == 1;
+}
+
+/**
+ * @brief Reads n bytes from the socket
+ *
+ * @param connfd the file descriptor of the socket
+ * @param buf the buffer to store the data
+ * @param n the number of bytes to read
+ * @return int32_t the number of bytes read
+ */
+static int32_t read_all(int connfd, char *buf, size_t n) {
+  size_t bytesRead = 0;
+  while (n > 0) {
+    int rv = read(connfd, buf, n);
+    if (rv < 0) {
+      perror("read() error");
+      return -1;
+    } else if (rv == 0) {
+      break;
+    } else {
+      assert((size_t)rv <= n);
+      n -= (size_t)rv;
+      bytesRead += (size_t)rv;
+      buf += rv;
+    }
+  }
+  return bytesRead;
+}
+
+/**
+ * @brief Writes n bytes to the socket
+ *
+ * @param connfd the file descriptor of the socket
+ * @param buf the buffer to write to the socket
+ * @param n the number of bytes to write
+ * @return int32_t 0 on success, -1 on failure
+ */
+static int32_t write_all(int connfd, char *buf, size_t n) {
+  while (n > 0) {
+    int rv = write(connfd, buf, n);
+    if (rv < 0) {
+      perror("write() error");
+      return -1;
+    } else {
+      assert((size_t)rv <= n);
+      n -= (size_t)rv;
+      buf += rv;
+    }
+  }
+  return 0;
+}
+
+/**
+ * @brief Reads a request from the client and sends a response
+ *
+ * @param connfd the file descriptor of the socket
+ * @return int32_t 0 on success, -1 on failure
+ */
+int32_t query(int connfd, const char *message) {
+  int32_t length = (int32_t)strlen(message);
+  if (length > MAX_MSG_SIZE) {
+    printf(
+        "Payload size too high! Please limit the message size to 4096 bytes!");
+    return -1;
+  }
+  char wbuf[4 + length];
+
+  // we need to send the data in network byte order
+  if (!isLittleEndian()) {
+    memcpy(&wbuf, &length, 4);
+  } else {
+    int32_t networkLength = htonl(length);
+    memcpy(&wbuf, &networkLength, 4);
+  }
+  memcpy(&wbuf[4], message, length);
+  int32_t rv = write_all(connfd, wbuf, 4 + length);
+  if (rv < 0) {
+    return -1;
+  }
+
+  char rbuf[4 + 4096 + 1] = {};
+  rv = read_all(connfd, rbuf, 4);
+  if (rv < 0) {
+    return -1;
+  } else if (rv != 4) {
+    if (rv == 0) {
+      printf("Connection closed by the server.\n");
+    } else {
+      LOG_ERROR("Premature EOF reached.");
+    }
+    return -1;
+  }
+
+  length = 0;
+  memcpy(&length, rbuf, 4);
+  if (isLittleEndian()) {
+    length = ntohl(length);
+  }
+  rv = read_all(connfd, &rbuf[4], length);
+  if (rv < 0) {
+    return -1;
+  } else if (rv != (size_t)length) {
+    LOG_ERROR("Premature EOF reached.");
+    return 0;
+  }
+  rbuf[4 + length] = '\0';
+  printf("The server says: %s\n", &rbuf[4]);
+  return 0;
+}
+
 int main() {
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
-        perror("Error creating a socket");
-        exit(EXIT_FAILURE);
-    }
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd < 0) {
+    perror("Error creating a socket");
+    exit(EXIT_FAILURE);
+  }
 
-    struct sockaddr_in addr = {};
-    addr.sin_family = AF_INET;
-    addr.sin_port = ntohs(1234);
-    addr.sin_addr.s_addr = ntohl(0);
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(1234);
+  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
-    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
-    if (rv) {
-        perror("Could not connect to the client");
-        exit(EXIT_FAILURE);
-    }
+  int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+  if (rv) {
+    perror("Could not connect to the client");
+    exit(EXIT_FAILURE);
+  }
 
-    char msg[] = "nah";
-    ssize_t n = write(fd, msg, sizeof(msg));
-    if (n < 0) {
-        perror("write() error");
-    }
-
-    char rbuf[64] = {};
-    n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        perror("read() error");
-        return 1;
-    }
-    printf("The server says: %s\n", rbuf);
+  rv = query(fd, "hello server!");
+  if (rv) {
     close(fd);
+    return -1;
+  }
+  rv = query(fd, "What is your name?");
+  if (rv) {
+    close(fd);
+    return -1;
+  }
+  rv = query(fd, "What is your name?");
+  if (rv) {
+    close(fd);
+    return -1;
+  }
+  close(fd);
+  return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,180 +1,187 @@
+#include <arpa/inet.h>
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
 #include <netinet/in.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+#define LOG_ERROR(fmt)                                                         \
+  (fprintf(stderr, "[ERROR] %s:%d(): " fmt "\n", __FILE__, __LINE__))
 
 // max request size
 const size_t MAX_MSG_SIZE = 4096;
 
+/**
+ * @brief Reads n bytes from the socket
+ *
+ * @param connfd the file descriptor of the socket
+ * @param buf the buffer to store the data
+ * @param n the number of bytes to read
+ * @return int32_t the number of bytes read
+ */
 static int32_t read_all(int connfd, char *buf, size_t n) {
-    while (n > 0) {
-        int rv = read(connfd, buf, n);
-        if (rv < 0) {
-            perror("read() error");
-            return -1;
-        }
-        else if (rv == 0) {
-            printf("EOF reached.");
-            return -1;
-        }
-        else {
-            // macro that checks if the conditions is true or false
-            // if false, throws an error and terminates the program
-            assert((size_t)rv <= n);
-            n -= (size_t)rv;
-            buf += rv;
-        }
+  size_t bytesRead = 0;
+  while (n > 0) {
+    int rv = read(connfd, buf, n);
+    if (rv < 0) {
+      perror("read() error");
+      return -1;
+    } else if (rv == 0) {
+      break;
+    } else {
+      assert((size_t)rv <= n);
+      n -= (size_t)rv;
+      bytesRead += (size_t)rv;
+      buf += rv;
     }
-    return 0;
+  }
+  return bytesRead;
 }
 
+/**
+ * @brief Writes n bytes to the socket
+ *
+ * @param connfd the file descriptor of the socket
+ * @param buf the buffer to write to the socket
+ * @param n the number of bytes to write
+ * @return int32_t 0 on success, -1 on failure
+ */
 static int32_t write_all(int connfd, char *buf, size_t n) {
-    while (n > 0) {
-        int rv = write(connfd, buf, n);
-        if (rv < 0) {
-            perror("write() error");
-            return -1;
-        }
-        else {
-            assert((size_t)rv <= n);
-            n -= (size_t)rv;
-            buf += rv;
-        }
+  while (n > 0) {
+    int rv = write(connfd, buf, n);
+    if (rv < 0) {
+      perror("write() error");
+      return -1;
+    } else {
+      assert((size_t)rv <= n);
+      n -= (size_t)rv;
+      buf += rv;
     }
-    return 0;
+  }
+  return 0;
 }
 
 static bool isLittleEndian() {
-    uint32_t test = 1;
-    return (*((unsigned char *)&test) == 1);
+  uint32_t test = 1;
+  return (*((unsigned char *)&test) == 1);
 }
 
+/**
+ * @brief Reads a request from the client and sends a response
+ *
+ * @param connfd the file descriptor of the socket
+ * @return int32_t 0 on success, -1 on failure
+ */
 static int32_t one_request(int connfd) {
-    // 4 bytes length header
-    char rbuf[4 + MAX_MSG_SIZE + 1] = {};
+  // 4 bytes length header
+  char rbuf[4 + MAX_MSG_SIZE + 1] = {};
 
-    int32_t rv = read_all(connfd, rbuf, 4);
-    if (rv) {
-        perror("Exiting due to read() error.");
-        exit(EXIT_FAILURE);
+  // read the length of the message
+  int32_t rv = read_all(connfd, rbuf, 4);
+  if (rv < 0) {
+    return -1;
+  } else if (rv != 4) {
+    if (rv == 0) {
+      printf("Connection closed by the client.\n");
+    } else {
+      LOG_ERROR("Premature EOF reached.");
     }
+    return -1;
+  }
 
-    // we assume that the bytes are transmitted in network order format
-    // i.e big endian, so we check if the system is little endian and
-    // store accordingly
-    int32_t length = 0;
-    if (isLittleEndian()) {
-        length = (rbuf[3] << 24 | rbuf[2] << 16 | rbuf[1] << 8 | rbuf[0]);
-    }
-    else {
-        memcpy(&length, rbuf, 4);
-    }
+  // convert the length to host byte order
+  int32_t length = 0;
+  memcpy(&length, rbuf, 4);
+  length = ntohl(length);
 
-    rv = read_all(connfd, &rbuf[4], length);
-    if (rv) {
-        perror("Exiting due to read() error.");
-        exit(EXIT_FAILURE);
-    }
-    rbuf[4 + length] = '\0';
-    printf("The server says: %sn", &rbuf[4]);
+  // read the message
+  rv = read_all(connfd, &rbuf[4], length);
+  if (rv < 0) {
+    return -1;
+  } else if (rv != (size_t)length) {
+    LOG_ERROR("Premature EOF reached.");
+    return 0;
+  }
+  rbuf[4 + length] = '\0';
+  printf("The client says: %s\n", &rbuf[4]);
 
-    char response[] = "Hi Server!";
-    char wbuf[4 + strlen(response)];
-    length = (uint32_t)strlen(response);
+  char response[] = "Hi Client!";
+  char wbuf[4 + strlen(response)];
+  length = (uint32_t)strlen(response);
+  if (length > MAX_MSG_SIZE) {
+    printf("Payload size too high! Please limit the message size to 4096 "
+           "bytes!\n");
+    return -1;
+  }
+
+  // make sure to send the data in network byte order
+  if (!isLittleEndian()) {
     memcpy(&wbuf, &length, 4);
-    memcpy(&wbuf, &response, length);
-    return write_all(connfd, wbuf, 4 + length);
+  } else {
+    int32_t networkLength = htonl(length);
+    memcpy(&wbuf, &networkLength, 4);
+  }
+  memcpy(&wbuf[4], response, length);
+  return write_all(connfd, wbuf, 4 + length);
 }
 
 int main() {
 
-    // create a socket and assign it a file descriptor
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // check if the socket was created successfully
-    if (fd < 0) {
+  if (fd < 0) {
+    perror("Socket creation failed");
+    exit(EXIT_FAILURE);
+  }
 
-        // print an error message and exit the program
-        // perror prints the error message to stderr,
-        // given string followed by a colon and then
-        // the error message corresponding to the errorno
-        // variable, which is a global variable that stores the
-        // error code of the last system call that failed
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
+  int val = 1;
+
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+  struct sockaddr_in server_addr = {};
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(1234);
+  server_addr.sin_addr.s_addr = htonl(0);
+
+  int rv = bind(fd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+
+  if (rv < 0) {
+    perror("Binding failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // 5 is the length of the waiting queue
+  rv = listen(fd, 5);
+  if (rv < 0) {
+    perror("Can't listen");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    struct sockaddr client_addr = {};
+    socklen_t socklen = sizeof(client_addr);
+
+    int32_t connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+
+    if (connfd < 0) {
+      perror("Can't eastablish the connection");
+      exit(EXIT_FAILURE);
     }
 
-    int val = 1;
-
-    // Used to set options for a socket
-    // SO_REUSEADDR, allows the socket to bind to a recently closed
-    // port while is is still in TIME_WAIT state, the port goes
-    // into this state to ensure that all the packets are taken
-    // care of and any late packets are discarded
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
-
-    // structure used to represent IPv4 socket address
-    // has 3 fields
-    // sin_family: address family, IPv4 or IPv6
-    // sin_port: the port number to bind the socket to
-    // sin_addr.s_addr: the IP to bind the socket to
-    struct sockaddr_in server_addr = {};
-
-    server_addr.sin_family = AF_INET;
-
-    // we need the port number in network byte order(always big endian)
-    // so ntohs converts it from host to network byte order
-    server_addr.sin_port = ntohs(1234);
-
-    // binds the socket to wildcard address 0.0.0.0
-    // tells the OS to bind the socket to all the available
-    // interfaces on the machine, this allows the server to accept
-    // connections from any interface
-    server_addr.sin_addr.s_addr = ntohl(0);
-
-    int rv = bind(fd, (const struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    // if binding fails for some reason
-    if (rv < 0) {
-        perror("Binding failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // 5 is the length of the waiting queue
-    rv = listen(fd, 5);
-    if (rv < 0) {
-        perror("Can't listen");
-        exit(EXIT_FAILURE);
-    }
-
+    // we can only serve one client connection at any time
     while (1) {
-        // accept a connection
-        struct sockaddr client_addr = {};
-        socklen_t socklen = sizeof(client_addr);
-
-        // fd for the connection
-        int32_t connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
-
-        if (connfd < 0) {
-            perror("Can't eastablish the connection");
-            exit(EXIT_FAILURE);
-        }
-
-        // we can only serve one client connection at any time
-        // until we have (event loops)?
-        while (1) {
-            int32_t err = one_request(connfd);
-        }
-        close(connfd);
+      int32_t err = one_request(connfd);
+      if (err < 0)
+        break;
     }
-
+    close(connfd);
+  }
+  return 0;
 }
